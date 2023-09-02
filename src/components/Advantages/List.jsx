7b76c8a3676104bdf92762d9f6098962b8a311cf
PetroYurchuk <petroyurchuk66@gmail.com>
2023-09-03 01:40:09 +0300

    add padding for advantages section

--- src/components/Advantages/List.jsx
+++ src/components/Advantages/List.jsx
@@ -2,7 +2,7 @@ import advantagesData from "../../utils/AdvantagesData";
 import ListItem from "./ListItem";
 const List = () => {
   return (
-    <ol className="list-decimal flex flex-col items-center">
+    <ol className="list-decimal flex flex-col items-center px-[10px]">
       {advantagesData.map(({ id, advantage }) => (
         <ListItem key={id} advantage={advantage} />
       ))}
